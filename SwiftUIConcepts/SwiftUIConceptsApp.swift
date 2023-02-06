8c6ad46ffb89eb37b4a77793834369b8bf2c52c1
dodor <doris.choi92@gmail.com>
2023-02-06 11:24:48 +0900

    Initial Commit

---
+++ SwiftUIConcepts/SwiftUIConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConceptsApp.swift
+//  SwiftUIConcepts
+//
+//  Created by Do-Young Choi on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
